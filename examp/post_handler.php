a5222cc2ad68dfb9efa9e29be424d0b3bc68b087
Vlad <ejiek@mail.ru>
2015-03-23 22:04:14 +0300

    added examp/post_handler.php added form for it in index.php

---
+++ examp/post_handler.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>
+			Test page N4
+		</title>
+	</head>
+	<body>
+		<?php
+			echo "<h1>Hello, " . $_POST["username"] . "</h1>";
+		?>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 			<li><a href="examp/constant.php">Constant and defined</a></li>
 			<li><a href="examp/func.php">Function</a></li>
 			<li><a href="examp/get_url.php?username=Vlad">$_GET (this link sends "Vlad")</a></li>
+			<li><form method="post" action="examp/post_handler.php">
+			<input type="text" name="username">
+			<input type="submit" value="$_POST"></li>
 		</ul>
 	</body>
 </html>

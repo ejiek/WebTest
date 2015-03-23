d4622df27db2673a642b96a4bef764345815d600
Vlad <ejiek@mail.ru>
2015-03-23 21:44:08 +0300

    added examp/get_url.php added link to it on index.php

---
+++ examp/get_url.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>
+			Test page N3
+		</title>
+	</head>
+	<body>
+		<?php
+			echo "<h1>Hello, " . $_GET["username"] . "</h1>";
+		?>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<ul>
 			<li><a href="examp/constant.php">Constant and defined</a></li>
 			<li><a href="examp/func.php">Function</a></li>
+			<li><a href="examp/get_url.php?username=Vlad">$_GET (this link sends "Vlad")</a></li>
 		</ul>
 	</body>
 </html>

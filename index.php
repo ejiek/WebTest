0dbc1a0233f3e32d2212aaf847125d375979ac44
Vlad <ejiek@mail.ru>
2015-03-24 08:44:37 +0300

    fixes unclosed <form> added id for post_form

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
 	session_destroy();
-
-	?>
+?>
 
 <html>
 	<head>
@@ -13,13 +11,15 @@
 			<li><a href="examp/constant.php">Constant and defined</a></li>
 			<li><a href="examp/func.php">Function</a></li>
 			<li><a href="examp/get_url.php?username=Vlad">$_GET (this link sends "Vlad")</a></li>
-			<li><form method="post" action="examp/post_handler.php">
-			<input type="text" name="username">
-			<input type="submit" value="$_POST"></li>
+			
+			<li><form id="post_form" method="post" action="examp/post_handler.php">
+			<input type="text" name="username" form="post_form">
+			<input type="submit" value="$_POST" form="post_form"></form></li>
+			
 			<li><form method="post" action="examp/session_login.php">	
-				<p><input type="text" name="username" placeholder="Username" ></p>
-				<input type="text" name="password" placeholder="Password" formaction="examp/session_login.php">
-				<input type="submit" formaction="examp/session_login.php" value="Open Session"></li>
+				<p><input type="text" name="username" placeholder="Username"></p>
+				<input type="text" name="password" placeholder="Password">
+				<input type="submit" value="Open Session"></form></li>
 		</ul>
 	</body>
 </html>
